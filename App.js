d60230cafa2e557e59e9b29b553ead357b6dfb99
wangshibiao <645102170@qq.com>
2019-11-29 19:07:13 +0800

    feat: 为导航菜单设置图标

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View, Button } from 'react-native';
+import { Platform, StyleSheet, Text, View, Button, Image } from 'react-native';
 import ExpoDeviceInfoView from "./component/ExpoDeviceInfoView";
 import NativeDeviceInfoView from "./component/NativeDeviceInfoView";
 import ExpoVideoPlayer from "./component/ExpoVideoPlayer";
@@ -44,24 +44,44 @@ const Navigator = createBottomTabNavigator({
     Home: {
         screen: App,
         navigationOptions: {
-            title: "主页"
+            title: "主页",
+            tabBarIcon: ({tintColor, focused}) => (
+                <Image style={{ width: 24, height: 24 }}
+                source={{ uri: 'http://img.taopic.com/uploads/allimg/120712/201731-120G2144H051.jpg' }}
+                />
+            ),
         }},
     ExpoDeviceInfoView: {
         screen: ExpoDeviceInfoView,
         navigationOptions: {
-            title: "调用expo的api"
+            title: "调用expo的api",
+            tabBarIcon: ({tintColor, focused}) => (
+                <Image style={{ width: 24, height: 24 }}
+                       source={{ uri: 'http://bpic.588ku.com/element_origin_min_pic/16/08/24/2257bdb209b22b0.jpg!/fwfh/804x764/quality/90/unsharp/true/compress/true' }}
+                />
+            ),
         }
     },
     NativeDeviceInfoView: {
         screen: NativeDeviceInfoView,
         navigationOptions: {
-            title: "调用native module的api"
+            title: "调用native module的api",
+            tabBarIcon: ({tintColor, focused}) => (
+                <Image style={{ width: 24, height: 24 }}
+                       source={{ uri: 'http://pic02.1sucai.com/190315/330828-1Z3151S03945-lp.jpg' }}
+                />
+            ),
         }
     },
     ExpoVideoPlayer: {
         screen: ExpoVideoPlayer,
         navigationOptions: {
-            title: "调用expo的播放器3"
+            title: "调用expo的播放器3",
+            tabBarIcon: ({tintColor, focused}) => (
+                <Image style={{ width: 24, height: 24 }}
+                       source={{ uri: 'https://pic02.taopic.com/191005/240494-1910051K05353-lp.jpg' }}
+                />
+            ),
         }
     },
 });
