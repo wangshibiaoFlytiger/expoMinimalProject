380f94476f68f8f639f63e9daced370c29c665e8
wangshibiao <645102170@qq.com>
2019-12-05 15:06:17 +0800

    feat: 删除Home导航菜单以及对应页面组件

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View, Button, Image } from 'react-native';
+import React from 'react';
+import { Image } from 'react-native';
 import ExpoDeviceInfoView from "./component/ExpoDeviceInfoView";
 import NativeDeviceInfoView from "./component/NativeDeviceInfoView";
 import ExpoVideoPlayer from "./component/ExpoVideoPlayer";
@@ -8,34 +8,6 @@ import {createAppContainer} from "react-navigation";
 import NewsListView from "./component/NewsListView";
 import {createStackNavigator} from "react-navigation-stack";
 import NewsDetailView from "./component/NewsDetailView";
-
-class App extends Component {
-    /**
-     * 跳转到指定页面
-     */
-    navigate(pageName){
-        this.props.navigation.navigate(pageName);
-    }
-
-  render() {
-    return (
-      <View style={styles.container}>
-          <Button onPress={this.navigate.bind(this, "ExpoDeviceInfoView")} title={"调用expo的api"}/>
-          <Button onPress={this.navigate.bind(this, "NativeDeviceInfoView")} title={"调用native module的api"}/>
-          <Button onPress={this.navigate.bind(this, "ExpoVideoPlayer")} title={"调用expo的播放器"}/>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-});
 
 // 定义新闻页面导航菜单
 const newsNavigator = createStackNavigator({
@@ -55,16 +27,6 @@ const newsNavigator = createStackNavigator({
 
 // 定义主导航菜单
 const Navigator = createBottomTabNavigator({
-    Home: {
-        screen: App,
-        navigationOptions: {
-            title: "主页",
-            tabBarIcon: ({tintColor, focused}) => (
-                <Image style={{ width: 24, height: 24 }}
-                source={{ uri: 'http://img.taopic.com/uploads/allimg/120712/201731-120G2144H051.jpg' }}
-                />
-            ),
-        }},
     News: {
         // 嵌套新闻页面导航
         screen: newsNavigator,
